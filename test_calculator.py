08ed185368e3d908da768b402e15637c2384db7a
vkumaw <vaidehikumawat2020@gmail.com>
2026-03-23 10:16:13 +0530

    Update test_calculator.py

--- test_calculator.py
+++ test_calculator.py
@@ -1,7 +1,7 @@
 from calculator import add, multiply, divide
 import pytest
 def test_add():
-	assert add(2,3) == 10
+	assert add(2,3) == 5
 def test_multiply():
 	assert multiply(3,4) == 12
 def test_divide():
